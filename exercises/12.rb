5193a930ef5309d12b530124a6c7752f4b0bba85
Boruch <bdakter@gmail.com>
2019-06-20 11:21:55 -0400

    added 12.rb

---
+++ exercises/12.rb
@@ -0,0 +1,24 @@
+# require 'pry'
+
+contact_data = [
+  ["joe@email.com", "123 Main st.", "555-123-4567"],
+  ["sally@email.com", "404 Not Found Dr.", "123-234-3454"],
+  ["napoleons@bakery.com", "Arc De Triomphe", "000-000-0000"]
+]
+
+contacts = {
+  "Joe Smith" => {},
+  "Sally Johnson" => {},
+  "Napoleon Bone Apart" => {}
+}
+
+contacts.each do |k, v|
+  current_data = contact_data.shift
+  while current_data.size > 0
+    v["email"] = current_data.shift
+    v["address"] = current_data.shift
+    v["phone"] = current_data.shift
+  end
+end
+
+contacts.each {|c| p c}
